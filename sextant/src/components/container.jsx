3550af89235363c6d91f776abe026339dd8b077e
Manu Kumar Pal <manukumarpal34@gmail.com>
2023-02-21 15:34:08 +0530

    modified task 2

--- sextant/src/components/container.jsx
+++ sextant/src/components/container.jsx
@@ -1,10 +1,14 @@
-import Subcontainer from"./Subcontainer.jsx"
+import React from 'react'
+import Subcontainer from './Subcontainer.jsx'
 function Container() {
   return (
     <div className='container'>
-        <Subcontainer name="IPv4" url="https://api.ipify.org?format=json"/>
-        <Subcontainer name="IPv6" url="https://api64.ipify.org?format=json"/>
+        <Subcontainer/>
+        <Subcontainer/>
+        <Subcontainer/>
+        <Subcontainer/>
     </div>
   )
 }
+
 export default Container
--- sextant/src/components/subcontainer.jsx
+++ sextant/src/components/subcontainer.jsx
@@ -1,10 +1,9 @@
-import React, { useEffect, useState } from 'react'
-import Container from './Container.jsx'
-import IpFinder from './IpFinder.jsx'
-function Subcontainer(prop) {
-    return (
-        <div className='Subcontainer'>{prop.name}
-        <div>IP :{<IpFinder url={prop.url}/>}</div></div>
-      )
-    }   
-    export default Subcontainer
+import React from 'react'
+
+function Subcontainer() {
+  return (
+    <div className='Subcontainer'>Subcontainer</div>
+  )
+}
+
+export default Subcontainer
